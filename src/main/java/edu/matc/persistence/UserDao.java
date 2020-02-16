49236900d5c270093e564573df3d02fda9d51dbf
bethscholze <bascholze@madisoncollege.edu>
2020-02-16 13:00:50 -0600

    Bookshelf Banshee added CRUD function bodies to userDoa

--- src/main/java/edu/matc/persistence/UserDao.java
+++ src/main/java/edu/matc/persistence/UserDao.java
@@ -3,8 +3,13 @@ package edu.matc.persistence;
 import edu.matc.entity.User;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
 
 public class UserDao {
@@ -13,24 +18,51 @@ public class UserDao {
     SessionFactory sessionFactory = SessionFactoryProvider.getSessionFactory();
 
     public User getById(int id) {
-        return User;
+        Session session = sessionFactory.openSession();
+        User user = session.get(User.class, id);
+        session.close();
+        return user;
     }
 
     public void saveOrUpdate(User user) {
-
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.saveOrUpdate(user);
+        transaction.commit();
+        session.close();
     }
 
     public int insert(User user) {
-        return 0;
+        int id = 0;
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        id = (int)session.save(user);
+        transaction.commit();
+        session.close();
+        return id;
     }
 
     public void delete(User user) {
-
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.delete(user);
+        transaction.commit();
+        session.close();
     }
 
     public List<User> getAll() {
+        //get session out of session factory
+        Session session = sessionFactory.openSession();
+        //used for get all users, and ones with where clauses
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+        //use it to create a query for a specific entity and specify on which class the query will execute
+        CriteriaQuery<User> query = builder.createQuery(User.class);
+        //like from portion of query
+        Root<User> root = query.from(User.class);
+        List<User> users = session.createQuery(query).getResultList();
+        session.close();
 
-        return null;
+        return users;
     }
 
 }
