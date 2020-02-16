3b860e310c0518b8a6ac5706b0eefc82e4ad9cf8
bethscholze <bascholze@madisoncollege.edu>
2020-02-16 13:20:26 -0600

    Added the Databse class and properties file for it. Added log4j properties file.

--- src/main/java/edu/matc/entity/Role.java
+++ src/main/java/edu/matc/entity/Role.java
@@ -1,4 +1,85 @@
 package edu.matc.entity;
-// TODO add stuff to this
+
+/**
+ * The type Role.
+ */
 public class Role {
+    private String role;
+    private String username;
+    private int id;
+
+    /**
+     * Gets role.
+     *
+     * @return the role
+     */
+    public String getRole() {
+        return role;
+    }
+
+    /**
+     * Gets username.
+     *
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Sets role.
+     *
+     * @param role the role
+     */
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    /**
+     * Sets username.
+     *
+     * @param username the username
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * Sets id.
+     *
+     * @param id the id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * Instantiates a new Role.
+     */
+    public Role() {
+    }
+
+    /**
+     * Instantiates a new Role.
+     *
+     * @param role     the role
+     * @param username the username
+     * @param id       the id
+     */
+    public Role(String role, String username, int id) {
+        this.role = role;
+        this.username = username;
+        this.id = id;
+    }
+
+    //TODO to string method?
 }
